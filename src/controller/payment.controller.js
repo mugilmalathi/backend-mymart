c4c5b734a1574b8565981e1bd902801ddd2d454d
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-05-25 14:00:50 +0530

    payment integrated checking

--- src/controller/payment.controller.js
+++ src/controller/payment.controller.js
@@ -1,7 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const {v4:uuidv4} = require("uuid");
+const stripe = require("stripe")("sk_test_51L3F1zSAb8TFHtBOPYPCTXRmzGNffA54GKdoVfV9dlrlDzJZ4HDGytCxwufrQ2JhZUQNyHwB4MUg7siUliI4lwx300XCNpMV7M")
 
 router.post("/payment", async(req, res)=>{
-    console.log(req.body);
-    res.send("Working properly..!")
+    const {product, token} = req.body;
+    const transactionKey = uuidv4();
+    return stripe.customers.create({
+        email: token.email,
+        source: token.id
+    }).then((customer)=>{
+        stripe.charges.create({
+            amount:product.price,
+            currency: "inr",
+            customer: customer.id,
+            receipt_email: token.email,
+            description: product.name
+        }).then((res)=>{
+            res.json(res)
+        }).catch((err)=>{
+            console.log(err);
+            
+        })
+    })
 })
+
+
+export default router;
